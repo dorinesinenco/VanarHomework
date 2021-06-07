7f29f1b3f147b4c3820939849e6273f315c6f3b9
sergiupirlici <sergiupirlici@gmail.com>
2021-06-07 11:12:25 +0300

    third

---
+++ 1_tryexcept.py
@@ -0,0 +1,13 @@
+def getData(index):
+    global data
+    data = [10, 20, 30, 40, 50]
+    return data[index]
+
+
+try:
+    index = int(input("Enter an index: "))
+    print(getData(index))
+except ValueError:
+    print("Index must be an integer")
+except IndexError:
+    print("Index can't be larger than " + str(len(data)-1))
